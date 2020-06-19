ca1e1997446738c1918a34cd750b78680d9edddd
Neo Si Hao <neosihao@gmail.com>
2020-06-19 15:04:51 +0800

    update title animation

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,22 +7,16 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        var charIndex = 0.0
-        titleLabel.text = ""
-        let titleText = "⚡FlashChat"
-        for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1.0
-        }
+        
+        titleLabel.text = "⚡FlashChat"
     }
     
 
