3a4c0446602df698a5f2979d38a8e208d487a7e5
Neo Si Hao <neosihao@gmail.com>
2020-06-20 00:10:53 +0800

    add login and signup authentication

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Neo Si Hao on 20/6/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+struct Constants {
+    static let registerSegue = "registerToChat"
+    static let loginSegue = "loginToChat"
+}
